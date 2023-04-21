97ffba1518081f52ceece54121f57ff928d83e81
Mariyan IT <97658816+MariyanDG@users.noreply.github.com>
2023-04-21 12:48:35 +0300

    Add files via upload

---
+++ Restricting Data_LAB/IS_NULL_Operator.sql
@@ -0,0 +1,9 @@
+
+SELECT * FROM employees 
+WHERE commission_pct = NULL;
+
+SELECT * FROM employees 
+WHERE commission_pct IS NULL;
+
+SELECT * FROM employees 
+WHERE commission_pct IS NOT NULL;
---
+++ Restricting Data_LAB/Using_WHERE_Clause.sql
@@ -0,0 +1,8 @@
+
+SELECT * FROM employees;
+
+SELECT * FROM employees 
+WHERE salary > 10000;
+
+SELECT * FROM employees 
+WHERE job_id = 'IT_PROG';
