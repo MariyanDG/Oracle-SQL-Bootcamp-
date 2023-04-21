07c42e5b4f4d39c02c11c1c50e1c5bf1b2f0d6de
Mariyan IT <97658816+MariyanDG@users.noreply.github.com>
2023-04-21 13:32:48 +0300

    Add files via upload

---
+++ Conversion Functions_LAB/Conversion_Functions.sql
@@ -0,0 +1,12 @@
+
+--Implicit Conversion FROM a VARCHAR2 value TO a "NUMBER" value.
+SELECT * FROM EMPLOYEES WHERE salary > '5000';
+
+--Implicit Conversion FROM a VARCHAR2 value TO a "DATE" value. 
+SELECT * FROM EMPLOYEES WHERE HIRE_DATE = '17-JUN-03';
+
+--Implicit Conversion FROM a NUMBER value TO a VARCHAR2 value. 
+SELECT DEPARTMENT_ID || DEPARTMENT_NAME FROM DEPARTMENTS;
+
+--Implicit Conversion FROM a DATE value TO a VARCHAR2 value. 
+SELECT FIRST_NAME || SYSDATE FROM EMPLOYEES;
---
+++ Conversion Functions_LAB/TO_CHAR_TO_DATE_TO_NUMBER_Functions_2.sql
@@ -0,0 +1,18 @@
+
+SELECT salary*commission_pct*100 "Original", 
+       TO_CHAR(salary*commission_pct*100, '$999,999.00') "Formatted Version"
+FROM employees WHERE commission_pct IS NOT NULL;
+
+SELECT salary*commission_pct*100 "Original", 
+       TO_CHAR(salary*commission_pct*100, 'L999,999.00') "Formatted Version"
+FROM employees WHERE commission_pct IS NOT NULL;
+
+SELECT salary*commission_pct*100 "Original", 
+       TO_CHAR(salary*commission_pct*100, '$099,999.00') "Formatted Version"
+FROM employees WHERE commission_pct IS NOT NULL;
+
+SELECT salary*commission_pct*100 "Original", 
+       TO_CHAR(salary*commission_pct*100, '$009,999.00') "Formatted Version"
+FROM employees WHERE commission_pct IS NOT NULL;
+
+SELECT to_number('$5,322.33', '$99,999.00') formatted_number FROM dual;
